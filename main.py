66c709f221b64fd53f7011d49c16975f331220e7
Dai Zuozhuo <daizuozhuo@gmail.com>
2014-02-20 21:16:06 +0800

    read file into one matrix

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 #!/opt/local/bin/python
-from numpy import *
+import numpy as np
+from sklearn.preprocessing import Imputer
+
+TAGMAP = [dict() for i in range(40)]
 
 def file2matrix(filename):
     fr = open(filename)
     fr.readline()
     arrayOLines = fr.readlines()
     rows = len(arrayOLines)
-    returnMat = zeros((rows, 190))
-    returnTag = []
+    returnMat = np.zeros((rows, 230))
+    returnTag = [[] for i in range(40)]
     index = 0
+    #read numerical data into returnMat and Tag into returnTag
     for line in arrayOLines:
-        words = line.rstrip('\n').split('\t')
-        #to do: replace missing value
-        returnMat[index, :] = map(lambda x: x if x.isdigit() else -1, 
-                                  words[:190])
-        returnTag.append(words[190:])
+        words = line.rstrip('\r\n').split('\t')
+        returnMat[index, :190] = map(
+            lambda x: float(x) if x.isdigit() else np.nan, words[:190])
+        for i in range(40):
+            returnTag[i].append(words[190+i])
         index += 1
-        #print returnMat[0], returnTag[0]
-    return returnMat, returnTag
+    
+    #define tagmap from tag to number
+    for i in range(40):
+        s = set(returnTag[i])
+        index = 1
+        for tag in s:
+            TAGMAP[i][tag] = index
+            index += 1
+
+    #transform tag into number and add into returnMat 
+    for i in range(rows):
+        for j in range(40):
+            returnMat[i][j] = TAGMAP[j][returnTag[j][i]]
+    
+    #replcacec missing values into mean of values
+    imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+    imp.fit(returnMat[:1000])
+    returnMat = imp.transform(returnMat)
+    return returnMat
+
 
 if __name__ == "__main__":
     file2matrix("data/orange_small_test.data")
